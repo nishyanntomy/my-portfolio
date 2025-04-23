53b722c54027eb7b411bcd3bef476176c50d3eaf
nishyanntomy <nishyanntomy@gmail.com>
2025-04-23 10:51:49 -0500

    add contact section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
+import { SkillsSection } from "@/sections/Skills";
+import { ContactSection } from "@/sections/Contact";
+import { FooterSection } from "@/sections/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <HeroSection />
       <ProjectsSection />
       <TapeSection />
+      <SkillsSection />
+      <ContactSection />
+      <FooterSection />
     </div>
   );
 }
